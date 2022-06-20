8da640594f7fd450fb41da106f67916c2e09b774
Dimas Ananda <traperwaze@gmail.com>
2022-06-20 09:37:09 +0700

    auto delete older histories if more than 50 saved
    delete the older histories associated with a link,
    save 50 later histories only.
    
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- __test__/history.model.test.js
+++ __test__/history.model.test.js
@@ -1,30 +1,64 @@
-const { nanoid } = require("nanoid");
-const { db, getModels, init } = require("../app/database/models/index");
+const { nanoid } = require('nanoid');
+const { db, getModels, init } = require('../app/database/models/index');
 
-describe("Model: Link", () => {
-  test("create new Link", async () => {
+describe('Model: Link', () => {
+  test('create new Link', async () => {
     await init();
 
     const id = nanoid();
-    const a = await getModels()["link"].create({
+    const a = await getModels()['link'].create({
       linkId: id,
     });
 
     // find the created link
-    const b = await getModels()["link"].findOne({ where: { linkId: id } });
+    const b = await getModels()['link'].findOne({ where: { linkId: id } });
     expect(b).toBeTruthy();
 
-    
     // create history associated with id
-    const c = await getModels()["history"].create({
-        linkId: b.id,
-        data: {
-            method: "GET",
-        }
+    const c = await getModels()['history'].create({
+      linkId: b.id,
+      data: {
+        method: 'GET',
+      },
     });
 
     expect(c).toBeTruthy();
 
-    a.destroy();
+    await a.destroy();
+  });
+
+  test('delete older history, keep 50 only', async () => {
+    await init();
+
+    const id = nanoid();
+    const a = await getModels()['link'].create({
+      linkId: id,
+    });
+
+    // find the created link
+    const b = await getModels()['link'].findOne({ where: { linkId: id } });
+    expect(b).toBeTruthy();
+
+    // create history associated with id 55 times
+    for (let i = 0; i < 55; i++) {
+      const c = await getModels()['history'].create({
+        linkId: b.id,
+        data: {
+          method: 'GET',
+        },
+      });
+    }
+
+    // find all histories
+    const histories = await getModels()['history'].findAll({
+      where: {
+        linkId: b.id,
+      },
+    });
+
+    // expect there are only 50 histories
+    expect(histories.length).toBe(50);
+
+    await a.destroy();
   });
 });
--- app/database/models/history.js
+++ app/database/models/history.js
@@ -9,12 +9,27 @@ module.exports = (sequelize) => {
     },
   }, { sequelize, modelName: 'history' });
   
-  History.getHistoryByLinkId = function (linkId) {
+  History.afterCreate((history, options) => {
+    // check if the history count is more than 50
+    // delete the older histories
+    const popHistory = async () => {
+      const histories = await History.getHistoryByLinkId(history.linkId, 'ASC');
+      if (histories.length > 50) {
+        const oldestHistory = histories[0];
+        await oldestHistory.destroy();
+        await popHistory();
+      }
+    }
+     
+    return popHistory();
+  });
+
+  History.getHistoryByLinkId = function (linkId, order = 'DESC') {
     return this.findAll({
       where: {
         linkId,
       },
-      order: [['updatedAt', 'DESC']],
+      order: [['updatedAt', order]],
     });
   }
 
